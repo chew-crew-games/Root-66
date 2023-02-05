963b29c1f63aab912b7c941b05f071be078c3e38
bigcookie <5748299+biggestcookie@users.noreply.github.com>
2023-02-05 04:50:17 -0500

    Added sound files. Main scene now plays music on awake

---
+++ Assets/Sounds/AudioTrigger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+/*[SerializeField] private AudioClip _ClipToPlay;
+
+private void OnTriggerEnter(Collider other){
+    if (other.tag == "Player"){
+    AudioManager.Instance.PlayVoiceOver(_ClipToPlay);
+  }
+}*/
