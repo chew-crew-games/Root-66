30c842c9ea203f8bb3ff1b0cf648cf02e3466377
biggestcookie <5748299+biggestcookie@users.noreply.github.com>
2023-02-05 15:55:14 -0500

    Cactus spawner

--- Assets/Scripts/Props/Prop.cs
+++ Assets/Scripts/Props/Prop.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class Prop : MonoBehaviour {
   Rigidbody rb;
-  [SerializeField] float initialVelocity = -4f;
-  public float viewVelocity;
+  [SerializeField] float initialVelocity = -20f;
   void Start() {
     rb = GetComponent<Rigidbody>();
   }
 
   void FixedUpdate() {
-    viewVelocity = initialVelocity + VehicleManager.globalVelocity;
     rb.velocity = new Vector3(
       0,
       0,
       initialVelocity + VehicleManager.globalVelocity
-    ) * Time.deltaTime;
+    );
   }
 }
